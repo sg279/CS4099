6199508a34977c346451e62060fc4f40501d0bd4
sg279 <38112626+sg279@users.noreply.github.com>
2020-10-24 21:47:42 +0100

    init

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import os
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.metrics import CategoricalAccuracy
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import *
+from tensorflow.keras.applications import *
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras import backend as k
 from matplotlib import pyplot as plt
 
 def make_model(input_shape, num_classes):
@@ -63,18 +72,102 @@ def make_model(input_shape, num_classes):
     outputs = layers.Dense(units, activation=activation)(x)
     return keras.Model(inputs, outputs)
 
-def mias_conversion():
+# hyper parameters for model
+nb_classes = 3  # number of classes
+based_model_last_block_layer_number = 126  # value is based on based model selected.
+img_width, img_height = 299, 299  # change based on the shape/structure of your images
+batch_size = 16  # try 4, 8, 16, 32, 64, 128, 256 dependent on CPU/GPU memory capacity (powers of 2 values).
+nb_epoch = 50  # number of iteration the algorithm gets trained.
+learn_rate = 1e-4  # sgd learning rate
+momentum = .9  # sgd momentum to avoid local minimum
+transformation_ratio = .05  # how aggressive will be the data augmentation/transformation
+def train(train_data_dir, validation_data_dir, model_path):
+    # Pre-Trained CNN Model using imagenet dataset for pre-trained weights
+    base_model = Xception(input_shape=(img_width, img_height, 3), weights='imagenet', include_top=False)
+
+    # Top Model Block
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    predictions = Dense(nb_classes, activation='softmax')(x)
+
+    # add your top layer block to your base model
+    model = Model(base_model.input, predictions)
+    print(model.summary())
+
+    # # let's visualize layer names and layer indices to see how many layers/blocks to re-train
+    # # uncomment when choosing based_model_last_block_layer
+    # for i, layer in enumerate(model.layers):
+    #     print(i, layer.name)
+
+    # first: train only the top layers (which were randomly initialized)
+    # i.e. freeze all layers of the based model that is already pre-trained.
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    # Read Data and Augment it: Make sure to select augmentations that are appropriate to your images.
+    # To save augmentations un-comment save lines and add to your flow parameters.
+    train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                       rotation_range=transformation_ratio,
+                                       shear_range=transformation_ratio,
+                                       zoom_range=transformation_ratio,
+                                       cval=transformation_ratio,
+                                       horizontal_flip=True,
+                                       vertical_flip=True)
+
+    validation_datagen = ImageDataGenerator(rescale=1. / 255)
+
+    os.makedirs(os.path.join(os.path.abspath(train_data_dir), '../preview'), exist_ok=True)
+    train_generator = train_datagen.flow_from_directory(train_data_dir,
+                                                        target_size=(img_width, img_height),
+                                                        batch_size=batch_size,
+                                                        class_mode='categorical')
+    # save_to_dir=os.path.join(os.path.abspath(train_data_dir), '../preview')
+    # save_prefix='aug',
+    # save_format='jpeg')
+    # use the above 3 commented lines if you want to save and look at how the data augmentations look like
+
+    validation_generator = validation_datagen.flow_from_directory(validation_data_dir,
+                                                                  target_size=(img_width, img_height),
+                                                                  batch_size=batch_size,
+                                                                  class_mode='categorical')
+
+    model.compile(optimizer='nadam',
+                  loss='categorical_crossentropy',  # categorical_crossentropy if multi-class classifier
+                  metrics=['accuracy', keras.metrics.AUC()])
+
+    # save weights of best training epoch: monitor either val_loss or val_acc
+
+    top_weights_path = os.path.join(os.path.abspath(model_path), 'top_model_weights.h5')
+    callbacks_list = [
+        ModelCheckpoint(top_weights_path, monitor='val_accuracy', verbose=1, save_best_only=True),
+        EarlyStopping(monitor='val_accuracy', patience=5, verbose=0)
+    ]
+
+    # Train Simple CNN
+    model.fit(train_generator, validation_data= validation_generator, epochs=nb_epoch, callbacks=callbacks_list)
+    # model.fit(train_generator, validation_split=0.2, epochs=nb_epoch, callbacks=callbacks_list)
+
+def mias_conversion(mode):
+
     data = pd.read_csv("mias.csv")
+    n = data.loc[data["severity"] == "N"]
+    m = data.loc[data["severity"]=="M"]
+    b = data.loc[data["severity"]=="B"]
+    if  mode == "val":
+        data = n[-round(0.2*len(n)):].append(m[-round(0.2*len(m)):]).append(b[-round(0.2*len(b)):])
+    elif mode == "train":
+        data = n[round(0.2*len(n)):].append(m[round(0.2*len(m)):]).append(b[round(0.2*len(b)):])
     path = ".\mias\\"
-    os.mkdir(path + "N")
-    os.mkdir(path + "B")
-    os.mkdir(path + "M")
+
+    os.makedirs(path +"\\"+mode+"\\N", exist_ok=True)
+    os.makedirs(path +"\\"+mode+"\\B", exist_ok=True)
+    os.makedirs(path +"\\"+mode+"\\M", exist_ok=True)
     for row in data.iterrows():
         Image.open(path + row[1]["reference_number"] + ".pgm").save(
-            os.path.join(path, row[1]["severity"], row[1]["reference_number"] + '.png'))
+            os.path.join(path,mode, row[1]["severity"], row[1]["reference_number"] + '.png'))
 
 def main():
-    image_size = (1024, 1024)
+    image_size = (180, 180)
     batch_size = 16
     from tensorflow.python.client import device_lib
     print(device_lib.list_local_devices())
@@ -87,6 +180,7 @@ def main():
         image_size=image_size,
         batch_size=batch_size,
     )
+    train_ds = tf.keras.applications.xception.preprocess_input(train_ds)
     val_ds = tf.keras.preprocessing.image_dataset_from_directory(
         "mias",
         validation_split=0.2,
@@ -101,6 +195,7 @@ def main():
     train_ds = train_ds.prefetch(buffer_size=16)
     val_ds = val_ds.prefetch(buffer_size=16)
     model = make_model(input_shape=image_size + (3,), num_classes=2)
+
     epochs = 50
 
     callbacks = [
@@ -108,8 +203,8 @@ def main():
     ]
     model.compile(
         optimizer=keras.optimizers.Adam(1e-3),
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        metrics=["accuracy"],
+        loss=CategoricalCrossentropy(),
+        metrics=[CategoricalAccuracy()],
     )
     model.fit(
         train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
@@ -117,4 +212,9 @@ def main():
     pass
 
 if __name__ == '__main__':
-    main()
+    # mias_conversion("val")
+    # mias_conversion("train")
+    # main()
+
+
+    train(".\mias\\train", ".\mias\\val", ".\model")
